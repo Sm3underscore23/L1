01d3b5ec15c79efd11dcfe04fef5031cc952a452
Sm3underscore23 <100semtim@mail.ru>
2025-08-30 21:11:28 +0300

    add readFromArray func

--- L1.9/main.go
+++ L1.9/main.go
@@ -11,17 +11,46 @@ import (
 	"time"
 )
 
-func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+func readFromArray(
+	ctx context.Context, cancel context.CancelFunc, interrupt chan os.Signal,
+	data [10]int, producerCh, consumerCh chan int,
+) {
+	go func() {
+		for _, x := range data {
+			select {
+			case <-ctx.Done():
+				close(producerCh)
+				return
+			case producerCh <- x:
+				time.Sleep(time.Millisecond * 300)
+			}
+		}
+		close(producerCh)
+	}()
 
-	data := [5]int{1, 2, 3, 4, 5}
+	go func() {
+		for x := range producerCh {
+			consumerCh <- x * 2
+		}
+		close(consumerCh)
+	}()
 
-	producerCh := make(chan int)
+	go func() {
+		<-interrupt
+		cancel()
+	}()
 
-	consumerCh := make(chan int)
+	for result := range consumerCh {
+		fmt.Println(result)
+	}
 
+	fmt.Println("done")
+}
+
+func generateFromArray(
+	ctx context.Context, cancel context.CancelFunc, interrupt chan os.Signal,
+	data [10]int, producerCh, consumerCh chan int,
+) {
 	wg := sync.WaitGroup{}
 
 	wg.Add(3)
@@ -57,4 +86,18 @@ func main() {
 	cancel()
 	wg.Wait()
 	fmt.Println("stopped")
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	data := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	producerCh := make(chan int)
+	consumerCh := make(chan int)
+
+	// readFromArray(ctx, cancel, interrupt, data, producerCh, consumerCh)
+	generateFromArray(ctx, cancel, interrupt, data, producerCh, consumerCh)
 }
