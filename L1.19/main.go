3fd8c360127f3e42e20f9562b42c0acd9ecb4957
Sm3underscore23 <100semtim@mail.ru>
2025-08-31 22:03:34 +0300

    task L1.19 done

---
+++ L1.19/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func reverce(data string) string {
+	if 2 > len(data) {
+		return data
+	}
+
+	result := []rune(data)
+
+	for i, j := 0, len(result)-1; i < j; {
+		result[i], result[j] = result[j], result[i]
+		i++
+		j--
+	}
+
+	return string(result)
+}
+
+func main() {
+	data := "главрыба"
+	fmt.Println(reverce(data))
+}
