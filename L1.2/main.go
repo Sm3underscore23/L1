d7ddf95e2258550537d6e1338f180ef3fc32227b
Sm3underscore23 <100semtim@mail.ru>
2025-09-16 11:23:12 +0300

    task L1.2 fix

--- L1.2/main.go
+++ L1.2/main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
+
+func square(data []int) {
+	wg := sync.WaitGroup{}
+
+	for _, num := range data {
+		wg.Add(1)
+		num := num
+		go func() {
+			defer wg.Done()
+			fmt.Println(num * num)
+		}()
+	}
+
+	wg.Wait()
+}
 
 func main() {
 	data := []int{2, 4, 6, 8, 1}
-	fmt.Println("atomic:", withAtomic(data))
-	fmt.Println("mutex:", withMutex(data))
-	fmt.Println("chan:", withChan(data))
+	square(data)
 }
